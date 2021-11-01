cc1eb59c535d65ee7e20840dcfbdfe0ef4264681
alexandru.oarna <alexandru.oarna@gmail.com>
2021-11-01 15:00:20 +0200

    Creating an Observable Service

--- angular-for-architects/src/app/app.module.ts
+++ angular-for-architects/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TryObservableServiceComponent } from './lab1/try-observable-service/try-observable-service.component';
+import { TryObservableServiceComponent } from './lab  Creating an Observable Service/try-observable-service/try-observable-service.component';
 import { ObservableStoreComponent } from './lab2/observable-store/observable-store.component';
 import { DataStore } from './lab2/state-management/observable-store/data.store';
 import { ObservableStoreButtonsComponent } from './lab2/observable-store-buttons/observable-store-buttons.component';
