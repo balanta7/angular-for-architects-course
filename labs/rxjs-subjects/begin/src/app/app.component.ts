a2d060157856b52499f537e6842de3d6014b962e
alexandru.oarna <alexandru.oarna@gmail.com>
2021-11-01 14:50:01 +0200

    Working with RxJS Subjects Exercise 2: Subscribing to an Observable

--- labs/rxjs-subjects/begin/src/app/app.component.ts
+++ labs/rxjs-subjects/begin/src/app/app.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SubjectsService } from './core/subjects.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   subscription1Data = [];
   subscription2Data = [];
   subscription3Data = [];
 
-  constructor() {}
+  private subscripitons = new Subscription();
+
+  constructor(private subjectsService : SubjectsService ) {}
+
+  ngOnDestroy(): void {
+    this.subscripitons.unsubscribe();
+  }
 
   ngOnInit() {}
 
   subscribe1() {
-
+    this.subscripitons.add(
+      this.subjectsService.observable$.subscribe(
+        (data) => this.subscription1Data.push(data)
+      )
+    );
   }
 
   subscribe2() {
