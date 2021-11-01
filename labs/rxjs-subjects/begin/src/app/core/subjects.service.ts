753fea0e9d6702de1433f2da82960b14dc7848c3
alexandru.oarna <alexandru.oarna@gmail.com>
2021-11-01 14:44:08 +0200

    Working with RxJS Subjects Exercise 1: Add RxJS Subject Functionality into a Service

--- labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
+++ labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
 
 
 @Injectable({
@@ -7,7 +8,8 @@ import { Injectable } from '@angular/core';
 export class SubjectsService {
   
   //Define Subject and Observable properties here
-
+  subject$: Subject<string>;
+  observable$: Observable<string>;
 
   constructor() {
     this.init();
@@ -15,10 +17,18 @@ export class SubjectsService {
 
   init() {
     // Create Subject and Observable Here 
-
+    this.subject$ = new Subject<string>();
+    this.observable$ = this.subject$.asObservable();
 
     // Create interval here
+    setInterval(() => {
+      const date = new Date();
 
+      // Place the following code on a single line when adding it 
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      
+      this.subject$.next(time);
+    }, 3000);
   }
 
 }
