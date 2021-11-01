31a27b17723ed017842b399c514b260f336708fe
alexandru.oarna <alexandru.oarna@gmail.com>
2021-11-01 15:07:11 +0200

    Working with State Management - Observable Store

--- angular-for-architects/src/app/app.module.ts
+++ angular-for-architects/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TryObservableServiceComponent } from './lab  Creating an Observable Service/try-observable-service/try-observable-service.component';
-import { ObservableStoreComponent } from './lab2/observable-store/observable-store.component';
-import { DataStore } from './lab2/state-management/observable-store/data.store';
-import { ObservableStoreButtonsComponent } from './lab2/observable-store-buttons/observable-store-buttons.component';
+import { ObservableStoreButtonsComponent } from './lab2 Working with State Management Observable Store/observable-store-buttons/observable-store-buttons.component';
+import { ObservableStoreComponent } from './lab2 Working with State Management Observable Store/observable-store/observable-store.component';
+import { DataStore } from './lab2 Working with State Management Observable Store/state-management/observable-store/data.store';
 
 @NgModule({
   declarations: [
